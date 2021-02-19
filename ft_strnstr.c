5f8bbc5b7d5d2294c71d01a2efeee712a9b0634f
Marce <lightsxworld@gmail.com>
2021-02-19 15:05:37 -0300

    magica do matheus

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 14:54:49 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/19 14:24:50 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:04:24 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,21 @@ char	*ft_strnstr(const char *src, const char *to_find, size_t size)
 	size_t			i;
 	size_t			j;
 	size_t			count;
-	unsigned int	flag;
 	char			*pointer;
 
 	i = 0;
-	j = 0;
-	flag = 0;
 	pointer = (char *)src;
 	if (!*to_find)
 		return ((char *)src);
 	while (src[i] != '\0' && i < size)
 	{
-		if (src[i] == to_find[j])
-		{
-			count = i;
-			while ((src[i] == to_find[j]) && to_find[j] != '\0' && i < size)
-			{
-				flag = 1;
-				i++;
-				j++;
-			}
-		}
-		if (to_find[j] == '\0' && flag)
+		j = 0;
+		count = i;
+		while ((src[i] == to_find[j]) && to_find[j] != '\0' && i++ < size)
+			j++;
+		if (to_find[j] == '\0')
 			return ((char *)&pointer[count]);
 		i++;
-		j = 0;
-		flag = 0;
 	}
 	return (NULL);
 }
