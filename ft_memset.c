7c45ee86f24f8b1d1cdd96adb75499cfaaa6ea88
Marce <lightsxworld@gmail.com>
2021-02-20 13:24:47 -0300

    pfvrvai

--- ft_memset.c
+++ ft_memset.c
@@ -6,25 +6,23 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 16:27:16 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/20 13:14:53 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/20 13:24:43 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *str, int c, size_t size)
 {
-	size_t	i;
-	char	cc;
-	char	*pointer_aux;
+	unsigned char	*s;
+	size_t			i;
 
+	s = (unsigned char *)str;
 	i = 0;
-	cc = (char)c;
-	pointer_aux = (char *)s;
-	while (i < n)
+	while (i < size)
 	{
-		pointer_aux[i] = cc;
+		s[i] = (unsigned char)c;
 		i++;
 	}
-	return ((void *)s);
+	return (str);
 }
