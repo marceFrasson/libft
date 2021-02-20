5c484b9b39e99ccac8bb5e7639af3f0b8d306ed5
Marce <lightsxworld@gmail.com>
2021-02-19 22:17:15 -0300

    trying

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 22:09:23 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/19 14:19:52 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/19 22:16:48 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,8 +75,7 @@ char			**ft_split(char const *str, char c)
 		return (NULL);
 	while (str[i])
 	{
-		while (str[i] == c)
-			i++;
+		while (str[i++] == c)
 		j = i;
 		while (str[i] && str[i] != c)
 			i++;
