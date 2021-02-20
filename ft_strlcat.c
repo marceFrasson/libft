3c1f27c482224ee1d093fa5b76be4d31f3c3f45a
Marce <lightsxworld@gmail.com>
2021-02-19 23:13:32 -0300

    pfvrvai

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 14:53:46 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/15 20:54:03 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/19 23:13:22 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,23 @@
 
 size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t len_dst;
+	size_t len_src;
 	size_t i;
 	size_t j;
-	size_t k;
 
-	i = 0;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	i = len_dst;
 	j = 0;
-	k = 0;
-	while (dst[i] != '\0')
-		i++;
-	while (src[k] != '\0')
-		++k;
-	if (size <= i)
-		k += size;
-	else
-		k += i;
-	while (src[j] != '\0' && i + 1 < size)
+	if (size <= len_dst)
+		return (len_src + size);
+	while (src[j] && i < (size - 1))
 	{
 		dst[i] = src[j];
 		i++;
 		j++;
 	}
 	dst[i] = '\0';
-	return (k);
+	return (len_dst + len_src);
 }
