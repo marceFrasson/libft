688e66be5890e56a4cfd036ed1ff321453515161
Marce <lightsxworld@gmail.com>
2021-02-19 21:33:08 -0300

    pfvrvai

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,12 +6,13 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 14:55:14 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/19 21:30:09 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/19 21:32:58 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
 int		ft_strncmp(const char *string1, const char *string2, size_t size)
 {
 	size_t			i;
@@ -26,6 +27,26 @@ int		ft_strncmp(const char *string1, const char *string2, size_t size)
 		if (str1[i] != str2[i])
 		{
 			return (str1[i] - str2[i]);
+		}
+		i++;
+	}
+	return (0);
+}*/
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*s1_cast;
+	unsigned char	*s2_cast;
+
+	i = 0;
+	s1_cast = (unsigned char *)s1;
+	s2_cast = (unsigned char *)s2;
+	while ((s1_cast[i] != '\0' || s2_cast[i] != '\0') && i < n)
+	{
+		if (s1_cast[i] != s2_cast[i])
+		{
+			return (s1_cast[i] - s2_cast[i]);
 		}
 		i++;
 	}
