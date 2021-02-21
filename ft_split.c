a87438870dacc92343c5c521ffd087576a82127a
Marce <lightsxworld@gmail.com>
2021-02-21 19:33:59 -0300

    trying

--- ft_split.c
+++ ft_split.c
@@ -6,42 +6,40 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 22:09:23 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/21 19:18:36 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/21 19:33:37 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	cwords(char const *s, char c)
+static int	word_count(char const *str, char c)
 {
-	int		i;
-	int		word;
-	int		count;
+	int i;
+	int count;
 
 	i = 0;
-	word = 0;
 	count = 0;
-	while (s[i])
+	while (str[i])
 	{
-		if (s[i] == c)
-			word = 0;
-		else if (s[i] != c && word == 0)
+		while (str[i] == c)
+			i++;
+		if (str[i] != c)
 		{
-			word = 1;
 			count++;
+			while (str[i] && str[i] != c)
+				i++;
 		}
-		i++;
 	}
 	return (count);
 }
 
-static char	*makeword(char const *s, char c)
+static char	*ft_putstr(char const *str, char c)
 {
 	int		i;
 	char	*word;
 
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (str[i] && str[i] != c)
 		i++;
 	word = (char *)malloc(i + 1);
 	if (!word)
@@ -50,32 +48,32 @@ static char	*makeword(char const *s, char c)
 	i--;
 	while (i >= 0)
 	{
-		word[i] = s[i];
+		word[i] = str[i];
 		i--;
 	}
 	return (word);
 }
 
-char		**ft_split(char const *s, char c)
+char		**ft_split(char const *str, char c)
 {
 	char	**split;
 	int		word;
 	int		count;
 	int		i;
 
-	if (!s || !(split = (char **)malloc((cwords(s, c) + 1) * sizeof(char *))))
+	if (!str || !(split = (char **)malloc((cwords(s, c) + 1) * sizeof(char *))))
 		return (NULL);
 	word = 0;
 	count = 0;
 	i = 0;
-	while (s[i])
+	while (str[i])
 	{
-		if (s[i] == c)
+		if (str[i] == c)
 			word = 0;
-		else if (s[i] != c && word == 0)
+		else if (str[i] != c && word == 0)
 		{
 			word = 1;
-			if (!(split[count] = makeword(&s[i], c)))
+			if (!(split[count] = ft_putstr(&str[i], c)))
 				return (NULL);
 			count++;
 		}
