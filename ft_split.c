a7c921142860e5325767b42c9cf5cea41c778579
Marce <lightsxworld@gmail.com>
2021-02-21 19:37:22 -0300

    trying

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 22:09:23 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/02/21 19:33:37 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/02/21 19:37:05 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	word_count(char const *str, char c)
+static int	wcnt(char const *str, char c)
 {
 	int i;
 	int count;
@@ -41,8 +41,7 @@ static char	*ft_putstr(char const *str, char c)
 	i = 0;
 	while (str[i] && str[i] != c)
 		i++;
-	word = (char *)malloc(i + 1);
-	if (!word)
+	if (!(word = (char *)malloc(i + 1)))
 		return (NULL);
 	word[i] = 0;
 	i--;
@@ -61,7 +60,7 @@ char		**ft_split(char const *str, char c)
 	int		count;
 	int		i;
 
-	if (!str || !(split = (char **)malloc((cwords(s, c) + 1) * sizeof(char *))))
+	if (!str || !(split = (char **)malloc((wcnt(s, c) + 1) * sizeof(char *))))
 		return (NULL);
 	word = 0;
 	count = 0;
